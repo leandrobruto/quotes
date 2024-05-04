0bf34481c0f51fa50371970a6e3c89fc1820f474
Leandro <leandrobruto42@gmail.com>
2024-05-04 20:47:16 -0300

    users, companies and, quotes associations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,11 +1,13 @@
 class Quote < ApplicationRecord
-    validates :name, presence: true
+  belongs_to :company
 
-    scope :ordered, -> { order(id: :desc) }
+  validates :name, presence: true
 
-    # after_create_commit -> { broadcast_prepend_later_to "quotes" }
-    # after_update_commit -> { broadcast_replace_later_to "quotes" }
-    # after_destroy_commit -> { broadcast_remove_to "quotes" }
-    # Those three callbacks are equivalent to the following single line
-    broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
